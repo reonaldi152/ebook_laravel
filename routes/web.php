2ec789ed00e604661159bfeb4f494c1036f5472a
reonaldi152 <reonaldi@team.superfk.co>
2024-09-26 20:46:08 +0700

    add view details

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FrontController;
 
 Route::get('/', [FrontController::class, 'index'])->name('front.index');
+
+Route::get('/details', function () {
+    return view('details');
+});
